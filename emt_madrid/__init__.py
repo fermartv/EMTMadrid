15cea35b94dddf9e3bb59010d483fc1d1678edec
Fernando Martínez <20268311+fermartv@users.noreply.github.com>
2025-07-19 11:29:58 +0200

    Make Line and Stop classes directly importable from emt_madrid package

--- emt_madrid/__init__.py
+++ emt_madrid/__init__.py
@@ -1,5 +1,7 @@
 """Wrapper for the Madrid EMT (Empresa Municipal de Trasnportes) API."""
 
 from .main import EMTClient
+from .domain.stop import Stop
+from .domain.line import Line
 
-__all__ = ["EMTClient"]
+__all__ = ["EMTClient", "Line", "Stop"]
